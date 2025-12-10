25b05788a40b8b47a21e922bf70c054b1151c2ca
Balti2003 <baltasarlomello@live.com>
2025-12-10 15:43:19 -0300

    commit 10/12

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../../services/products';
+import { ProductService } from '../../services/products';
 
 @Component({
   selector: 'app-products',
@@ -7,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './products.css',
 })
 export class Products {
+  productos: Product[] = [];
 
+  constructor(private product: ProductService) {
+    this.productos = this.product.getProducts();
+  }
 }
